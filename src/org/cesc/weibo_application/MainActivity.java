c0b58982966e7a6883331c333d5d6c0b560ebfa1
ceschere <longlingxiu@hotmail.com>
2012-07-22 23:49:49 +0800

    第一个次代码提交

--- src/org/cesc/weibo_application/MainActivity.java
+++ src/org/cesc/weibo_application/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        System.out.println("第一个哥的提交");
     }
 
     @Override
