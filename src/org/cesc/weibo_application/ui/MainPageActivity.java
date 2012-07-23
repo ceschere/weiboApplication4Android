ef65847caa109de1fb85a8ea0f3a947c52a7841e
ceschere <longlingxiu@hotmail.com>
2012-07-23 22:37:39 +0800

    添加主屏幕 和  主屏幕布局

---
+++ src/org/cesc/weibo_application/ui/MainPageActivity.java
@@ -0,0 +1,17 @@
+package org.cesc.weibo_application.ui;
+
+import org.cesc.weibo_application.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
